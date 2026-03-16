e3fa2772c1c175d39d5de49f6e85421277200ba0
ANIKETPROJECTS <aniketraneworkemail@gmail.com>
2026-03-16 17:00:04 +0530

    Change script path from index.cjs to index.js

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: "atdigitalmenu",
-    script: "./dist/index.cjs",
+    script: "./dist/index.js",
     instances: 1,
     autorestart: true,
     watch: false,
